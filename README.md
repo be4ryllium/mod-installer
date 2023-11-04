1d5ad818b31f616f3e92ccaced9eaf26d9b3f4b9
wapSekc <68976748+wapSekc@users.noreply.github.com>
2023-11-04 13:52:32 -0700

    updated file search, support for resource packs

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# mod installer lets you install minecraft mods directly into the mods folder instead of having to put them in manually
+# mod installer lets you install minecraft mods or resource packs directly into the mods folder instead of having to put them in manually
 
 good for people who are tired of navigating to the thingy every time you want to do a thing
 
@@ -11,4 +11,4 @@ this only works on windows because i dont feel like learning linux or mac<br>jus
 
 on first time installs just  execute "first_time_install" (make sure the folder is in the directory you want because thats how the command thing works and if you move the folder it wont work any more)
 
-you can change your mods folder directory in the "mods_folder_directory" text file (only the first line please)
+you can change your mods folder directory in the "mods_folder_directory" text file (only the first line please)<br>and your resource pack folder directory in the "resource_pack_folder_directory" text file
--- commands/python/mc.py
+++ commands/python/mc.py
@@ -2,12 +2,13 @@ import requests
 import sys
 import os
 
+if len(sys.argv) == 1:
+	print("mc <command> [args]\n\ncommands:\n  install <modrinth mod name>:\n  installs a mod off of modrinth and puts it into your mods folder\n  ex:\n    mc install sodium\n\n  update:\n    checks for updates in the program")
+	exit()
+
 thing_to_do = sys.argv[1]
 
-if thing_to_do == None:
-	print("mc <command> [args]\n\ncommands:\n  install <modrinth mod name>\n  ex:\n    mc install sodium\n\n  update")
-
-elif thing_to_do == 'update':
+if thing_to_do == 'update':
 	location = sys.argv[0]
 
 	# i know this is horrible shut up
@@ -26,7 +27,7 @@ elif thing_to_do == 'update':
 		answer = input("a new version is available, would you like to install? (y/n)\n")
 		if answer == 'y':
 			newest_version = requests.get("https://codeload.github.com/be4ryllium/mod-installer/zip/refs/heads/master").content
-			with open("newest.zip", mode="wb") as f:
+			with open("..\\newest.zip", mode="wb") as f:
 				f.write(newest_version)
 		else:
 			print("ok")
@@ -35,47 +36,58 @@ elif thing_to_do == 'install':
 
 	mod_name = sys.argv[2] # get the argument from the thing
 
-	mod = requests.get("https://api.modrinth.com/v2/project/" + mod_name)
+	print("getting from modrinth...")
+
+	mod = requests.get("https://api.modrinth.com/v2/project/" + mod_name + "/version")
 
 	if mod.status_code == 404:
-		print("Error: no mod with the name", mod_name, "exists.")
+		print("Error: no mod/resource pack with the name", mod_name, "exists.")
 	else:
 		mod_json = mod.json()
-		github_url = mod_json['source_url']
+		is_mod = False
 
-		if github_url.find("github") == -1:
-			print("This thing only works with github source links sorry :(")
+
+		v = 0
+		for i in mod_json:
+			print(str(v) + ": " + i['name'] + "  -  " + i['version_number'])
+		v += 1
+
+		answer = input("which one? (type 'x' to cancel): ")
+
+		if answer == "x":
+			print("cancelled")
+			exit()
+
+		mod_jar_name = mod_json[int(answer)]['files'][0]['filename']
+
+		if mod_jar_name.find(".jar") != -1:
+			print("project type: mod")
+			is_mod = True
 		else:
-			author_and_repo = github_url[18:len(github_url)]
+			print("project type: resource pack")
 
-			versions = requests.get("https://api.github.com/repos" + author_and_repo + "/releases")
-			versions_json = versions.json()
-
-			v = 0
-			for i in versions_json:
-				print(str(v) + ": " + i['name'])
-				v += 1
-
-			answer = input("which one? (type 'x' to cancel): ")
-
-			if answer == "x":
-				print("cancelled")
-				exit()
-
-			print("downloading...")
-			mod_name_with_version = versions_json[int(answer)]['name']
-			browser_download_url = versions_json[int(answer)]['assets'][0]['browser_download_url']
-			mod_jar = requests.get(browser_download_url).content
-			print("downloaded")
-
-			mod_jar_name = browser_download_url[browser_download_url.rfind("/")+1:len(browser_download_url)]
-
+		print("downloading...")
+		browser_download_url = mod_json[int(answer)]['files'][0]['url']
+		mod_jar = requests.get(browser_download_url).content
+		print("downloaded")
+		
+		if is_mod:
 			with open("../mods_folder_directory.txt") as f:
 				mod_folder_location = f.read()
 				if mod_folder_location == "":
 					mod_folder_location = os.getenv('APPDATA') + "\\.minecraft\\mods"
 				with open(mod_folder_location + "\\" + mod_jar_name,mode="wb") as jar:
 					jar.write(mod_jar)
-					print("done. downloaded to " + mod_folder_location + "\\" + mod_jar_name)
+					print("done. saved to " + mod_folder_location + "\\" + mod_jar_name)
+					jar.close()
+				f.close()
+		else:
+			with open("../resource_pack_folder_directory.txt") as f:
+				res_folder_location = f.read()
+				if res_folder_location == "":
+					res_folder_location = os.getenv('APPDATA') + "\\.minecraft\\resourcepacks"
+				with open(res_folder_location + "\\" + mod_jar_name,mode="wb") as jar:
+					jar.write(mod_jar)
+					print("done. saved to " + res_folder_location + "\\" + mod_jar_name)
 					jar.close()
 				f.close()
