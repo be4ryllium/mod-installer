4baa1eb1bd7e407c771438ab02d4b46053372ccb
wapSekc <68976748+wapSekc@users.noreply.github.com>
2023-11-05 15:03:54 -0800

    my c key is only slightly broken

--- commands/python/mc.py
+++ commands/python/mc.py
@@ -2,9 +2,134 @@ import requests
 import sys
 import os
 
+command_descs = [
+	"  install <project name> [-dep]:\n    installs a mod/resource pack off of modrinth and puts it into your mods/resource packs folder.\n    if -dep is enabled, it will only install the dependencies associated with the mod.\n    ex:\n      mc install sodium",
+	"  update:\n    checks for updates in the program",
+	"  describe <project name> [-in-detail]:\n    prints the description (or body, if you enable -in-detail) of the project\n    ex:\n      mc describe sodium -in-detail"
+]
+
 def explain():
-	print("\nmc <command> [args]\n\ncommands:\n  install <modrinth project name>:\n    installs a mod/resource pack off of modrinth and puts it into your mods/resource packs folder\n    ex:\n      mc install sodium\n\n  update:\n    checks for updates in the program")
+	print("\nmc <command> [args]\n\ncommands:")
+	for i in command_descs:
+		print(i + "\n")
 	exit()
+
+def install(mod_name = None, dep_only = None):
+	if mod_name == None:
+		mod_name = sys.argv[2] # get the argument from the thing
+
+	if len(sys.argv) > 3:
+		for i in sys.argv[3:len(sys.argv)]:
+			if i == '-dep' and dep_only == None:
+				dep_only = True
+				print("only getting dependencies from mod...")
+
+	print("getting from modrinth...")
+
+	mod = requests.get("https://api.modrinth.com/v2/project/" + mod_name + "/version")
+
+	if mod.status_code == 404:
+		print("no mod/resource pack with the name", mod_name, "exists.")
+	else:
+		mod_json = mod.json()
+		is_mod = False
+
+		v = 0
+		for i in mod_json:
+			versions_string = ''
+			loaders_string = ''
+			for g in i['game_versions']:
+				versions_string += g + ", "
+			for g in i['loaders']:
+				loaders_string += g + ", "
+			if versions_string != '':
+				versions_string = versions_string[0:len(versions_string)-2]
+			if loaders_string != '':
+				loaders_string = loaders_string[0:len(loaders_string)-2]
+			print(str(v) + ": " + i['name'] + "    -    " + versions_string + "    -    " + loaders_string)
+			v += 1
+
+		answer = input("which one? (type 'x' to cancel): ")
+
+		if answer == "x":
+			print("cancelled")
+			exit()
+
+		mod_jar_name = mod_json[int(answer)]['files'][0]['filename']
+
+		if mod_jar_name.find(".jar") != -1:
+			print("project type: mod")
+			is_mod = True
+		else:
+			print("project type: resource pack")
+			if dep_only:
+				print("error: dependency only mode doesn't work on resource packs")
+				exit()
+
+		if dep_only:
+			dependencies = mod_json[int(answer)]['dependencies']
+			if len(dependencies) == 0:
+				print("this mod doesn't have any dependencies")
+				exit()
+			dep_i = 0
+			for dep in dependencies:
+				dep_i += 1
+				print("getting dependency... (" + str(dep_i) + "/" + str(len(dependencies)) + ")\n")
+				depend_r = requests.get("https://api.modrinth.com/v2/project/" + dep['project_id']).json()
+				answer = input("name:        " + depend_r['title'] + "\ndescription: " + depend_r['description'] + "\nimportance:  " + dep['dependency_type'] + "\n\nwould you like to install? (y/n/x)\n")
+				if answer == 'y':
+					install(dep['project_id'], False)
+				elif answer == 'x':
+					print("aborted")
+					exit()
+				elif answer == 'n':
+					print("ok")
+
+		print("downloading...")
+		browser_download_url = mod_json[int(answer)]['files'][0]['url']
+		mod_jar = requests.get(browser_download_url).content
+		print("downloaded")
+		
+		if is_mod:
+			with open("../mods_folder_directory.txt") as f:
+				mod_folder_location = f.read()
+				if mod_folder_location == "":
+					mod_folder_location = os.getenv('APPDATA') + "\\.minecraft\\mods"
+				with open(mod_folder_location + "\\" + mod_jar_name,mode="wb") as jar:
+					jar.write(mod_jar)
+					print("done. saved to " + mod_folder_location + "\\" + mod_jar_name)
+					jar.close()
+				f.close()
+
+			dependencies = mod_json[int(answer)]['dependencies']
+			if len(dependencies) > 0:
+				answer = input("\nthis mod has some dependencies, would you like to see and decide on installing them one by one? (y/n)\n")
+				if answer == 'y':
+					dep_i = 0
+					for dep in dependencies:
+						dep_i += 1
+						print("getting dependency... (" + str(dep_i) + "/" + str(len(dependencies)) + ")\n")
+						depend_r = requests.get("https://api.modrinth.com/v2/project/" + dep['project_id']).json()
+						answer = input("name:        " + depend_r['title'] + "\ndescription: " + depend_r['description'] + "\nimportance:  " + dep['dependency_type'] + "\n\nwould you like to install? (y/n/x)\n")
+						if answer == 'y':
+							install(dep['project_id'], False)
+						elif answer == 'x':
+							print("aborted")
+							break
+						elif answer == 'n':
+							print("ok")
+				else:
+					print("ok")
+		else:
+			with open("../resource_pack_folder_directory.txt") as f:
+				res_folder_location = f.read()
+				if res_folder_location == "":
+					res_folder_location = os.getenv('APPDATA') + "\\.minecraft\\resourcepacks"
+				with open(res_folder_location + "\\" + mod_jar_name,mode="wb") as jar:
+					jar.write(mod_jar)
+					print("done. saved to " + res_folder_location + "\\" + mod_jar_name)
+					jar.close()
+				f.close()
 
 if len(sys.argv) == 1:
 	explain()
@@ -39,68 +164,22 @@ if thing_to_do == 'update':
 		print("up to date")
 
 elif thing_to_do == 'install':
+	install()
 
-	mod_name = sys.argv[2] # get the argument from the thing
+elif thing_to_do == 'describe':
+	mod_name = sys.argv[2]
+	in_detail = 'description'
 
-	print("getting from modrinth...")
+	if len(sys.argv) > 3:
+		for i in sys.argv[3:len(sys.argv)]:
+			if i == '-in-detail':
+				in_detail = 'body'
 
-	mod = requests.get("https://api.modrinth.com/v2/project/" + mod_name + "/version")
-
-	if mod.status_code == 404:
-		print("Error: no mod/resource pack with the name", mod_name, "exists.")
+	mod_req = requests.get("https://api.modrinth.com/v2/project/" + mod_name)
+	if mod_req.status_code == 404:
+		print("no project exists with the name", mod_name)
 	else:
-		mod_json = mod.json()
-		is_mod = False
+		print(mod_req.json()[in_detail])
 
-
-		v = 0
-		for i in mod_json:
-			versions_string = ''
-			for g in i['game_versions']:
-				versions_string += g + ", "
-			if versions_string != '':
-				versions_string = versions_string[0:len(versions_string)-2]
-			print(str(v) + ": " + i['name'] + "    -    " + versions_string)
-			v += 1
-
-		answer = input("which one? (type 'x' to cancel): ")
-
-		if answer == "x":
-			print("cancelled")
-			exit()
-
-		mod_jar_name = mod_json[int(answer)]['files'][0]['filename']
-
-		if mod_jar_name.find(".jar") != -1:
-			print("project type: mod")
-			is_mod = True
-		else:
-			print("project type: resource pack")
-
-		print("downloading...")
-		browser_download_url = mod_json[int(answer)]['files'][0]['url']
-		mod_jar = requests.get(browser_download_url).content
-		print("downloaded")
-		
-		if is_mod:
-			with open("../mods_folder_directory.txt") as f:
-				mod_folder_location = f.read()
-				if mod_folder_location == "":
-					mod_folder_location = os.getenv('APPDATA') + "\\.minecraft\\mods"
-				with open(mod_folder_location + "\\" + mod_jar_name,mode="wb") as jar:
-					jar.write(mod_jar)
-					print("done. saved to " + mod_folder_location + "\\" + mod_jar_name)
-					jar.close()
-				f.close()
-		else:
-			with open("../resource_pack_folder_directory.txt") as f:
-				res_folder_location = f.read()
-				if res_folder_location == "":
-					res_folder_location = os.getenv('APPDATA') + "\\.minecraft\\resourcepacks"
-				with open(res_folder_location + "\\" + mod_jar_name,mode="wb") as jar:
-					jar.write(mod_jar)
-					print("done. saved to " + res_folder_location + "\\" + mod_jar_name)
-					jar.close()
-				f.close()
 else:
 	explain()
